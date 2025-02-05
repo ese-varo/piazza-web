dc89d021606bddada7fdf512e7897918377f10d1
Alvaro Padilla <varoboss.ap@gmail.com>
2025-02-05 16:57:49 -0600

    Build the authentication system

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,17 @@ class User < ApplicationRecord
   validates :password,
     presence: true,
     length: { minimum: 8 }
+
+  has_many :app_sessions
+
+  def self.create_app_session(email:, password:)
+    user = User.authenticate_by(email: email, password: password)
+    user.app_sessions.create if user.present?
+  end
+
+  def authenticate_app_session(app_session_id, token)
+    app_sessions.find(app_session_id).authenticate_token(token)
+  rescue ActiveRecord::RecordNotFound
+    nil
+  end
 end
