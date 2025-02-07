123c4e97d2af620e6a2bc1624325e710dea5a276
Alvaro Padilla <varoboss.ap@gmail.com>
2025-02-07 12:58:41 -0600

    Add logout action

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -25,4 +25,15 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_select ".notification",
       I18n.t("sessions.create.incorrect_details")
   end
+
+  test "logging out redirects to the root url anad deletes the session" do
+    log_in(@user)
+
+    assert_difference("@user.app_sessions.count", -1) { log_out }
+    assert_redirected_to root_path
+
+    follow_redirect!
+    assert_select ".notification",
+      I18n.t("sessions.destroy.success")
+  end
 end
